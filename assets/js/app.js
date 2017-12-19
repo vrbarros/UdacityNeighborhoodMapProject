5c3c28ea89f876ff14bb2e487c47cc841afbe695
Victor Barros <victor.barros@windowslive.com>
2017-12-19 01:08:21 -0200

    All the changes after the last review.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,44 +7,39 @@ var Model = function () {
       title: 'Park Ave Penthouse',
       location: {
         lat: 40.7713024,
-        lng: -73.9632393
-      }
-    },
-    {
+        lng: -73.9632393,
+      },
+    }, {
       title: 'Chelsea Loft',
       location: {
         lat: 40.7444883,
-        lng: -73.9949465
-      }
-    },
-    {
+        lng: -73.9949465,
+      },
+    }, {
       title: 'Union Square Open Floor Plan',
       location: {
         lat: 40.7347062,
-        lng: -73.9895759
-      }
-    },
-    {
+        lng: -73.9895759,
+      },
+    }, {
       title: 'East Village Hip Studio',
       location: {
         lat: 40.7281777,
-        lng: -73.984377
-      }
-    },
-    {
+        lng: -73.984377,
+      },
+    }, {
       title: 'TriBeCa Artsy Bachelor Pad',
       location: {
         lat: 40.7195264,
-        lng: -74.0089934
-      }
-    },
-    {
+        lng: -74.0089934,
+      },
+    }, {
       title: 'Chinatown Homey Space',
       location: {
         lat: 40.7180628,
-        lng: -73.9961237
-      }
-    }
+        lng: -73.9961237,
+      },
+    },
   ];
 };
 
@@ -52,78 +47,125 @@ model = new Model();
 
 var AppViewModel = {
   // Load Locations
-  locations: ko.observableArray(model.Locations),
+  locations: ko.observableArray( model.Locations ),
+  filtered: ko.observableArray( [] ),
   // Search text input field
-  search: ko.observable(''),
-  shouldClear: ko.observable(false),
+  search: ko.observable( '' ),
+  filter: ko.observable( '' ),
+  shouldClear: ko.observable( false ),
 
   // Select location based on a list
-  selectLocation: function (data) {
+  selectLocation: function ( data ) {
     // Remove all markers from the map
-    neighborhoodmap.removeMarkers(markers);
+    neighborhoodmap.removeMarkers( markers );
+    // Create the marker on the map
+    neighborhoodmap.createMarker( data );
 
-    // This function create a market at the maps
-    var marker = new google.maps.Marker({
-      map: map,
-      title: data.title,
-      animation: google.maps.Animation.DROP,
-      position: data.location,
-    });
-
-    // Add the marker to the array
-    markers.push(marker);
-
-    // Change map position
-    map.setCenter(data.location);
+  },
+  loadMarkers: function ( array ) {
+    // Remove all markers from the map
+    neighborhoodmap.removeMarkers( markers );
+    // Loads the array in the map
+    neighborhoodmap.loadMarkers( array );
   },
   // Mark all favorite locations
   allFavorite: function () {
-    // Remove all markers from the map
-    neighborhoodmap.removeMarkers(markers);
-
-    // Set the bounds
-    var bounds = new google.maps.LatLngBounds();
-
-    // Iterate all Locations
-    ko.utils.arrayForEach(this.locations(), function (location) {
-      var marker = new google.maps.Marker({
-        map: map,
-        title: location.title,
-        animation: google.maps.Animation.DROP,
-        position: location.location,
-      });
-      // Add the marker to the array
-      markers.push(marker);
-
-      // Extend bounds
-      bounds.extend(location.location);
-    });
-
-    // Adjust the viewport
-    map.fitBounds(bounds);
+    // Clean the filtered and search fields
+    this.resetConsole();
+    // Load markers from all favorites
+    this.loadMarkers( this.locations() );
+  },
+  // Show favorite filtered on the map
+  filteredFavorites: function () {
+    this.loadMarkers( this.filtered() );
   },
   // Search console controls after changes in search input
   searchConsole: function () {
     // Set the state to show or hide
-    if (this.search() === '') {
+    if ( this.search() === '' ) {
       // Hide the clear button
-      this.shouldClear(false);
+      this.shouldClear( false );
     } else {
       // Show the clear button
-      this.shouldClear(true);
+      this.shouldClear( true );
+    }
+  },
+  // Start the favorite list
+  favoriteList: function () {
+    // Remove all items
+    this
+      .filtered
+      .removeAll();
+
+    for ( var i = 0; i < this.locations().length; i++ ) {
+      this
+        .filtered
+        .push( this.locations()[ i ] );
+    };
+  },
+  // Filter console controls after changes in filter input
+  filterConsole: function () {
+    filter = this.filter()
+    // Set the state to show or hide
+    if ( filter === '' ) {
+      // Hide the clear button
+      this.resetConsole();
+
+    } else {
+      // Show the clear button
+      this.shouldClear( true );
+
+      // Remove all items
+      this
+        .filtered
+        .removeAll();
+
+      // Filter each item testing characters
+      var test = ko
+        .utils
+        .arrayFilter( this.locations(), function ( item ) {
+          var title = item
+            .title
+            .toLowerCase();
+          if ( title !== 'undefined' ) {
+            return title.includes( filter );
+          } else {
+            return false;
+          }
+        } );
+
+      // If has some value found
+      if ( test.length > 0 ) {
+        // Save each result into the obsersavable array
+        for ( var i = 0; i < test.length; i++ ) {
+          this
+            .filtered
+            .push( test[ i ] );
+        };
+      }
+
+      // Show the filtered results
+      this.filteredFavorites( this.filtered() )
+
     }
   },
   // Clear the text from search input field
   clearSearch: function () {
+    this.resetConsole();
+  },
+  // Reset general console
+  resetConsole: function () {
     // Set text to empty
-    this.search('');
+    this.search( '' );
+    this.filter( '' );
     // Hide the clear button again
     this.searchConsole();
+    // Reload all favorites liste
+    this.favoriteList();
     // Remove all markers from the map
-    neighborhoodmap.removeMarkers(markers);
+    neighborhoodmap.removeMarkers( markers );
   }
-
 };
 
 // Data-binding the View Model object
-ko.applyBindings(AppViewModel);
+ko.applyBindings( AppViewModel );
--- assets/js/neighborhoodmap.js
+++ assets/js/neighborhoodmap.js
@@ -5,7 +5,9 @@ neighborhoodmap = {
   // Initial function from Google Maps javascript API
   initGoogleMaps: function () {
     // Set the Latitude and Longitude if the current user location is not available
-    var defaultLocation = new google.maps.LatLng(40.748817, -73.985428);
+    var defaultLocation = new google
+      .maps
+      .LatLng( 40.748817, -73.985428 );
 
     // Set the map options
     var mapOptions = {
@@ -19,110 +21,139 @@ neighborhoodmap = {
     }
 
     // Create map object
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    map = new google
+      .maps
+      .Map( document.getElementById( "map" ), mapOptions );
 
     // Create a generic info window to use when necessary
-    infoWindow = new google.maps.InfoWindow();
+    infoWindow = new google
+      .maps
+      .InfoWindow();
 
     // Create infoWindow object to show current user location
-    var currentLocation = new google.maps.InfoWindow();
+    var currentLocation = new google
+      .maps
+      .InfoWindow();
 
     // Check if browser and user has geolocation enabled
-    if (navigator.geolocation) {
+    if ( navigator.geolocation ) {
       // Get user current location
-      navigator.geolocation.getCurrentPosition(function (position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        // Open the infoWindow at the map
-        currentLocation.open(map);
-        // Set current location considering user location
-        currentLocation.setPosition(pos);
-        // Update infoWindow text
-        currentLocation.setContent('This is your position and your neighborhood.');
-        // Set the map current user location
-        map.setCenter(pos);
-      });
+      navigator
+        .geolocation
+        .getCurrentPosition( function ( position ) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          // Open the infoWindow at the map
+          currentLocation.open( map );
+          // Set current location considering user location
+          currentLocation.setPosition( pos );
+          // Update infoWindow text
+          currentLocation.setContent( 'This is your position and your neighborhood.' );
+          // Set the map current user location
+          map.setCenter( pos );
+        } );
     };
 
-
-
     // Create the search box and link it to the UI element.
-    var input = document.getElementById('search-text');
-    var searchBox = new google.maps.places.SearchBox(input);
+    var input = document.getElementById( 'search-text' );
+    var searchBox = new google
+      .maps
+      .places
+      .SearchBox( input );
 
     // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function () {
-      searchBox.setBounds(map.getBounds());
-    });
+    map.addListener( 'bounds_changed', function () {
+      searchBox.setBounds( map.getBounds() );
+    } );
 
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
-    searchBox.addListener('places_changed', function () {
+    searchBox.addListener( 'places_changed', function () {
       var places = searchBox.getPlaces();
 
-      if (places.length == 0) {
+      if ( places.length == 0 ) {
         return;
       }
 
       // Remove all markers from the map
-      neighborhoodmap.removeMarkers(markers);
+      neighborhoodmap.removeMarkers( markers );
 
       // For each place, get the icon, name and location.
-      var bounds = new google.maps.LatLngBounds();
-      places.forEach(function (place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
+      var bounds = new google
+        .maps
+        .LatLngBounds();
+      places.forEach( function ( place ) {
+        if ( !place.geometry ) {
+          console.log( "Returned place contains no geometry" );
           return;
         }
         // Create an special icon using the place type
         var icon = {
           url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
+          size: new google
+            .maps
+            .Size( 71, 71 ),
+          origin: new google
+            .maps
+            .Point( 0, 0 ),
+          anchor: new google
+            .maps
+            .Point( 17, 34 ),
+          scaledSize: new google
+            .maps
+            .Size( 25, 25 )
         };
 
         // Create a marker for each place
-        var marker = new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          id: place.place_id,
-          animation: google.maps.Animation.DROP,
-          position: place.geometry.location
-        });
+        var marker = new google
+          .maps
+          .Marker( {
+            map: map,
+            icon: icon,
+            title: place.name,
+            id: place.place_id,
+            animation: google.maps.Animation.DROP,
+            position: place.geometry.location
+          } );
 
         // Update info window content
 
-
         // Add click listener to marker to open info InfoWindow
-        marker.addListener('click', function () {
+        marker.addListener( 'click', function () {
 
           // Set the request with place id information
           var request = {
             placeId: place.place_id
           };
           // Create PlacesServices object to request from server
-          service = new google.maps.places.PlacesService(map);
-          service.getDetails(request, callback);
+          service = new google
+            .maps
+            .places
+            .PlacesService( map );
+          service.getDetails( request, callback );
 
           // Animation when clicked function
-          marker.setAnimation(google.maps.Animation.BOUNCE);
+          marker.setAnimation( google.maps.Animation.BOUNCE );
           // Set timeout to stop animation
-          setTimeout(function () {
-            marker.setAnimation(null);;
-          }, 2000);
+          setTimeout( function () {
+            marker.setAnimation( null );;
+          }, 2000 );
 
           // Function to callback if has success
-          function callback(place, status) {
+          function callback( place, status ) {
             // Check if has success and retunr info window
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
+            if ( status == google.maps.places.PlacesServiceStatus.OK ) {
               // Set variables from web webservice
-              var lat = place.geometry.location.lat()
-              var lng = place.geometry.location.lng()
+              var lat = place
+                .geometry
+                .location
+                .lat()
+              var lng = place
+                .geometry
+                .location
+                .lng()
               var name = place.name
               var address = place.formatted_address
               var phone = place.formatted_phone_number
@@ -130,45 +161,81 @@ neighborhoodmap = {
 
               // Set the place content information from the web service
               content = `
-                        <img style="float:left; width:200px; margin-top:30px"  src="https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${lat},${lng}&key=AIzaSyBd3n4_Gu9itUdXG7s67DtSWfkTeK2HnYI">
+                        <img style="float:left; width:200px; margin-top:30px"  src="https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${ lat},${ lng}&key=AIzaSyBd3n4_Gu9itUdXG7s67DtSWfkTeK2HnYI">
                         <div style="margin-left:220px; margin-bottom:20px;">
-                          <h2>${name}</h2><p></p>`
+                          <h2>${ name }</h2><p></p>`
               // Check if some variables exists
-              if (address) content += `<p><b>Address:</b> ${address}</p>`
-              if (phone) content += `<p><b>Phone:</b> ${phone}</p>`
-              if (website) content += `<p><b>Website:</b> <a target="_blank" href="${website}">Click here</a></p>`
-              // Closing the content
+              if ( address )
+                content += `<p><b>Address:</b> ${ address }</p>`
+              if ( phone )
+                content += `<p><b>Phone:</b> ${ phone }</p>`
+              if ( website )
+                content += `<p><b>Website:</b> <a target="_blank" href="${ website }">Click here</a></p>`
+                // Closing the content
               content += `</div>`;
 
               // Update the content into the info window
-              infoWindow.setContent(content);
+              infoWindow.setContent( content );
               // Open the info window with the content
-              infoWindow.open(map, marker);
+              infoWindow.open( map, marker );
             }
           }
 
-        });
+        } );
 
         // Add the marker to the array
-        markers.push(marker);
+        markers.push( marker );
 
         // Choose between location or viewport
-        if (place.geometry.viewport) {
+        if ( place.geometry.viewport ) {
           // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
+          bounds.union( place.geometry.viewport );
         } else {
-          bounds.extend(place.geometry.location);
+          bounds.extend( place.geometry.location );
         }
-      });
-      map.fitBounds(bounds);
-    });
+      } );
+      map.fitBounds( bounds );
+    } );
   },
-  removeMarkers: function (markers) {
+  removeMarkers: function ( markers ) {
     // This function will loop through the listings and hide them all.
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
+    for ( var i = 0; i < markers.length; i++ ) {
+      markers[ i ].setMap( null );
     }
     // Clear the array from markers
     markers = [];
   },
+  createMarker: function ( data ) {
+    // This function create a market at the maps
+    var marker = new google
+      .maps
+      .Marker( { map: map, title: data.title, animation: google.maps.Animation.DROP, position: data.location, } );
+    // Add infoWindow to the marker
+    marker.addListener( 'click', function () {
+      // Update the content into the info window
+      infoWindow.setContent( data.title );
+      // Open the info window with the content
+      infoWindow.open( map, marker );
+    } );
+    // Add the marker to the array
+    markers.push( marker );
+    // Change map position
+    map.setCenter( data.location );
+  },
+  loadMarkers: function ( data ) {
+    // Set the bounds
+    var bounds = new google
+      .maps
+      .LatLngBounds();
+    // Iterate all Locations
+    data.forEach( function ( location ) {
+      // Create each mark re-using the function create marker
+      neighborhoodmap.createMarker(location);
+      // Extend bounds
+      bounds.extend( location.location );
+    } );
+
+    // Adjust the viewport
+    map.fitBounds( bounds );
+  }
 }
